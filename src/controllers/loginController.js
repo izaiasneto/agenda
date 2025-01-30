f648ded03f52f3aac2f8e6bcc568ad0d0dc69175
Izaias Neto <izaiasmneto@gmail.com>
2025-01-29 21:53:52 -0300

    feat: create login controller and model

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,25 @@
+const Login = require('../models/LoginModel');
+
 exports.index = (req, res) => {
     res.render('login');
 }
+
+exports.register = async (req, res) => {
+    try {
+        const login = new Login(req.body);
+        await login.register();
+    
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(() => res.redirect('/login'));
+            return;
+        }
+    
+        req.flash('success', 'User has been created successfully');
+        req.session.save(() => res.redirect('/login'));
+        return;
+    } catch (e) {
+        console.log(e);
+        res.render('404');
+    }
+};
